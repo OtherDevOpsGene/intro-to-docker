a59a7303f61c47b88b28a75261179ced33086206
Gene Gotimer <eugene.gotimer@steampunk.com>
2020-11-02 13:07:52 -0500

    Prereqs done

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # intro-to-docker
-A hands-on introduction to using Docker
+
+A hands-on introduction to using Docker.
+
+## Prerequisites
+
+To participate, you need to have a workstation with Docker installed and you will have access to Docker. On a Linux system, this probably means you have to be in the `docker` group.
+
+If you want to use a fresh cloud image, set up an instance in AWS with a current Ubuntu AMI and then copy the [install-docker.sh](install-docker.sh) script to the system and run it as root (e.g., `sudo bash ./install-docker.sh`). A `t2.micro` (1 vCPU, 1 GiB RAM) or similar is probably sufficient for the Docker lessons.
+
+To complete the Docker Compose lesson, you'll need Docker Compose and the Redis CLI installed. The `install-docker.sh` script handles those. The cloud instance might need to be a  `t3a.medium` (2 vCPU, 4 GiB RAM) instance to handle all the containers concurrently running in that lesson.
+
+Ensure that you are ready by invoking the `docker` command from the command line.
+
+```bash
+docker --version
+```
+
+If the command works and you get a response similar to below, you are ready to proceed with [Lesson 1- Our First Containers](01-Lesson.md).
+
+```bash
+Docker version 19.03.13, build 4484c46d9d
+```
+
+The exact version and build number are not critical to this workshop.
---
+++ install-docker.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# To set up an Ubuntu system like we need for this workshop,
+# copy this script the the target system and run it as root:
+#     sudo bash ./install-docker.sh
+
+# You'll also need the following ports open:
+# * 22 for SSH traffic if the system is in the cloud
+# * 80, 4444, and 8080 open for HTTP traffic
+
+# Prerequisite software for Docker
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Use the Docker repository directly for the latest and greatest version
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+
+# Install Docker Community Edition and the Redis command-line interface
+apt-get install -y docker-ce redis-tools
+
+# Install Docker Compose (but not the Ubuntu package that requires an X11 display)
+curl -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Give the default ubuntu user permission to use Docker 
+usermod -a -G docker ubuntu
+# If you aren't using an AWS image with the ubuntu user, just replace the username with
+# the appropriate user or users on your system
